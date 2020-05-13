ec15245f72484fe5b6a5185a278520486ba1b2d0
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-05-13 15:31:48 +0200

    qutebrowser: Add support for sending a url to kodi for playback

---
+++ qutebrowser/userscripts/kodi-play-url.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# shellcheck disable=SC2153
+[ -z "${KODI_HOST}" ] && { echo "KODI_HOST is empty, cannot continue" ; exit 1; }
+[ -z "${KODI_PORT}" ] && { echo "KODI_PORT is empty, using 8080" ; KODI_PORT=8080; }
+
+STREAM_URL="$(youtube-dl -g "$1")"
+if [ -z "$STREAM_URL" ]; then
+    STREAM_URL="$1"
+fi
+
+echo curl "http://${KODI_HOST}:${KODI_PORT}/jsonrpc" \
+    -H "Content-Type: application/json" \
+    --data "{\"method\":\"Player.Open\",\"id\":44,\"jsonrpc\":\"2.0\",\"params\":{\"item\":{\"file\":\"${STREAM_URL}\"}}}"
