0dfc05511e1f9195712c49bea0460bf5bb0fc236
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2022-06-03 14:25:15 +0200

    qutebrowser: Switch yt-dlp in kodi-play-url.sh userscript

--- qutebrowser/userscripts/kodi-play-url.sh
+++ qutebrowser/userscripts/kodi-play-url.sh
@@ -3,11 +3,12 @@
 [ -z "${KODI_HOST}" ] && { echo "KODI_HOST is empty, cannot continue" ; exit 1; }
 [ -z "${KODI_PORT}" ] && { echo "KODI_PORT is empty, using 8080" ; KODI_PORT=8080; }
 
-STREAM_URL="$(youtube-dl -g "$1")"
+STREAM_URL="$(yt-dlp -g "$1")"
 if [ -z "$STREAM_URL" ]; then
     STREAM_URL="$1"
 fi
 
 curl "http://${KODI_HOST}:${KODI_PORT}/jsonrpc" \
     -H "Content-Type: application/json" \
+    --basic --user kodi:kodi \
     --data "{\"method\":\"Player.Open\",\"id\":44,\"jsonrpc\":\"2.0\",\"params\":{\"item\":{\"file\":\"${STREAM_URL}\"}}}"
